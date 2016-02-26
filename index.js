c7a021c0ade08584ebbc6962fdd8b7bdc8af5f98
Maros Kollar <maros@k-1.com>
2016-02-26 08:34:22 +0100

    Rework how random timeouts are handled

--- index.js
+++ index.js
@@ -17,7 +17,6 @@ function RandomDevice (id, controller) {
     // Call superconstructor first (AutomationModule)
     RandomDevice.super_.call(this, id, controller);
     
-    this.timeoutOff         = undefined;
     this.timeoutRollDice    = undefined;
 }
 
@@ -33,7 +32,6 @@ RandomDevice.prototype.init = function (config) {
     RandomDevice.super_.prototype.init.call(this, config);
     var self = this;
     
-    var currentTime = (new Date()).getTime();
     var langFile = self.controller.loadModuleLang("RandomDevice");
     
     // Create vdev
@@ -64,7 +62,7 @@ RandomDevice.prototype.init = function (config) {
             this.set("metrics:icon", "/ZAutomation/api/v1/load/modulemedia/RandomDevice/icon_"+command+".png");
             
             if (command === 'on') {
-                self.startRollDice();
+                self.rollDice();
             } else if (command === 'off') {
                 if (self.vDev.get('metrics:triggered') === true) {
                     self.randomOff();
@@ -75,35 +73,37 @@ RandomDevice.prototype.init = function (config) {
         moduleId: self.id
     });
     
+    setTimeout(_.bind(self.initCallback,self),60*1000);
+};
+
+RandomDevice.prototype.initCallback = function() {
+    var self = this;
+    
+    var currentTime = (new Date()).getTime();
+
     if (self.vDev.get('metrics:triggered') === true) {
         console.log('[RandomDevice] Init found triggered device');
         var offTime = self.vDev.get('metrics:offTime');
         if (offTime > currentTime) {
-            self.timeoutOff = setTimeout(
-                _.bind(self.randomOff,self),
-                (offTime-currentTime)
+            self.timeoutRollDice = setTimeout(
+                _.bind(self.rollDice,self), 
+                (offTime - currentTime)
             );
         } else {
-            self.randomOff();
+            self.rollDice();
         }
     } else {
+        self.rollDice();
         self.vDev.set("metrics:device",null);
         self.vDev.set("metrics:offTime",null);
         self.vDev.set("metrics:onTime",null);
     }
-    
-    self.startRollDice();
 };
 
 RandomDevice.prototype.stop = function() {
     var self = this;
     
     self.clearRollDice();
-    
-    if (typeof(self.timeoutOff) !== 'undefined') {
-        clearTimeout(self.timeoutOff);
-        self.timeoutOff = undefined;
-    }
     
     if (self.vDev) {
         self.controller.devices.remove(self.vDev.id);
@@ -125,129 +125,121 @@ RandomDevice.prototype.clearRollDice = function() {
     }
 };
 
-RandomDevice.prototype.startRollDice = function() {
-    var self = this;
-    
-    if (self.vDev.get('metrics:level') !== 'on') {
-        return;
-    }
-    
-    var interval    = parseInt(self.config.timeTo,10) - parseInt(self.config.timeFrom,10);
-    var seconds     = Math.round(Math.random() * interval * 60) + parseInt(self.config.timeFrom,10) * 60;
-    
-    console.log('[RandomDevice] Roll dice in '+(seconds));
-    self.clearRollDice();
-    self.timeoutRollDice = setTimeout(
-        _.bind(self.rollDice,self), 
-        1000*seconds
-    );
-};
-
 RandomDevice.prototype.rollDice = function () {
     var self=this;
     
     console.log('[RandomDevice] Roll dice');
     
+    var triggeredDevice;
     var currentTime     = (new Date()).getTime();
     var randomOn        = false;
-    var devicesConfig   = self.config.devices;
+    var devicesPool     = [];
     var triggered       = self.vDev.get('metrics:triggered');
+    var interval        = parseInt(self.config.timeTo,10) - parseInt(self.config.timeFrom,10);
+    var seconds         = Math.round(Math.random() * interval * 60) + parseInt(self.config.timeFrom,10) * 60;
     
-    self.startRollDice();
-    
-    if (triggered === true
-        && self.vDev.get('metrics:offTime') < currentTime) {
-        self.randomOff();
-        triggered = false;
+    self.clearRollDice();
+
+    // Roll dice again
+    if (self.vDev.get('metrics:level') === 'on') {
+        self.timeoutRollDice = setTimeout(
+            _.bind(self.rollDice,self), 
+            1000*seconds
+        );
     }
     
-    _.each(devicesConfig,function(deviceId) {
+    // Get device status
+    _.each(self.config.devices,function(deviceId) {
+        console.log('CHECK'+deviceId);
         var deviceObject = self.controller.devices.get(deviceId);
         if (deviceObject === null) {
             return;
         }
         
+        devicesPool.push(deviceObject);
+        
         var deviceLevel  = deviceObject.get('metrics:level');
         if (
             (
                 deviceObject.get('deviceType') === 'switchBinary' 
-                && deviceLevel === 'off'
+                && deviceLevel === 'on'
             )
             || 
             (
                 deviceObject.get('deviceType') === 'switchMultilevel'
-                && deviceLevel === 0
+                && deviceLevel > 0
             )) {
-            if (triggered === true
-                && self.vDev.get('metrics:device') === deviceId) {
-                self.randomOff();
+            
+            // Random device triggered
+            if (self.vDev.get('metrics:device') === deviceId) {
+                triggerDevice = deviceObject;
+            // Device on for some other reason
+            } else {
+                randomOn = true;
             }
-            return;
         }
-        randomOn = true;
     });
     
     // Check any device on
     if (randomOn) {
-        console.log('[RandomDevice] Random is already on');
+        console.log('[RandomDevice] A random device is already on');
+        return;
+    }
+    
+    if (devicesPool.length === 0) {
+        console.log('[RandomDevice] No device found');
         return;
     }
     
     // Check random device on
     if (self.vDev.get('metrics:level') !== 'on') {
         console.log('[RandomDevice] Random controller is off');
+        self.randomOff();
         return;
     }
     
     // Roll dice for trigger
     var randomTrigger = Math.round(Math.random() * 100);
     if (randomTrigger > self.config.probability) {
-        console.log('[RandomDevice] No matche');
+        console.log('[RandomDevice] No match');
         return;
     }
     
     // Roll dice for device
-    var randomDevice    = Math.round(Math.random() * (devicesConfig.length-1));
-    var deviceId        = devicesConfig[randomDevice];
-    
-    // Roll dice for duration
-    var interval        = parseInt(self.config.timeTo,10) - parseInt(self.config.timeFrom,10);
-    var minutes         = Math.round(Math.random() * interval) + parseInt(self.config.timeFrom,10);
-    var deviceObject    = self.controller.devices.get(deviceId);
-    var duration        = (minutes * 60 * 1000);
+    var randomIndex     = Math.round(Math.random() * (devicesPool.length-1));
+    var randomDevice    = devicesPool[randomIndex];
+    var duration        = (seconds * 1000);
     var offTime         = currentTime + duration;
     
-    if (deviceObject === null) {
-        console.error('[RandomDevice] No device for id '+deviceId);
-        return;
-    }
+    console.log('INDEX '+randomIndex);
+    console.logJS(devicesPool);
     
-    // Turn on device
-    console.log('[RandomDevice] Turning on random device '+deviceObject.id+' for '+minutes+' minutes');
-    if (deviceObject.get('deviceType') === 'switchBinary') {
-        deviceObject.performCommand('on');
-    } else if (deviceObject.get('deviceType') === 'switchMultilevel') {
-        deviceObject.performCommand('exact',{ level: 99 });
+    if (triggeredDevice.id === randomDevice.id) {
+        console.log('[RandomDevice] Extending random device '+randomDevice.id+' for another '+seconds+' seconds');
     } else {
-        console.error('[RandomDevice] Unspported device type '+deviceObject.get('deviceType'));
-        return;
+        if (typeof(triggeredDevice) !== 'undefined') {
+            triggeredDevice.set('metrics:auto',false);
+            triggeredDevice.performCommand('off');
+        }
+        
+        // Turn on device
+        console.log('[RandomDevice] Turning on random device '+randomDevice.id+' for '+seconds+' seconds');
+        if (randomDevice.get('deviceType') === 'switchBinary') {
+            randomDevice.performCommand('on');
+        } else if (randomDevice.get('deviceType') === 'switchMultilevel') {
+            randomDevice.performCommand('exact',{ level: 99 });
+        } else {
+            console.error('[RandomDevice] Unspported device type '+randomDevice.get('deviceType'));
+            return;
+        }
     }
     
-    deviceObject.set('metrics:auto',true);
-    
-    if (self.timeoutOff) {
-        clearTimeout(self.timeoutOff);
-    }
-    
-    self.timeoutOff = setTimeout(
-        _.bind(self.randomOff,self),
-        duration
-    );
+    randomDevice.set('metrics:auto',true);
     
     //self.clearRollDice();
     self.vDev.set("metrics:icon", "/ZAutomation/api/v1/load/modulemedia/RandomDevice/icon_triggered.png");
     self.vDev.set("metrics:triggered",true);
-    self.vDev.set("metrics:device",deviceObject.id);
+    self.vDev.set("metrics:device",randomDevice.id);
     self.vDev.set("metrics:offTime",offTime);
     self.vDev.set("metrics:onTime",currentTime);
 };
@@ -266,11 +258,6 @@ RandomDevice.prototype.randomOff = function() {
     
     deviceObject.performCommand('off');
     deviceObject.set('metrics:auto',false);
-    
-    if (self.timeoutOff) {
-        clearTimeout(self.timeoutOff);
-        self.timeoutOff = undefined;
-    }
     
     var level = self.vDev.get('metrics:level');
     self.vDev.set("metrics:icon", "/ZAutomation/api/v1/load/modulemedia/RandomDevice/icon_"+level+".png");
